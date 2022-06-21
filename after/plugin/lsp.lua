0d3ea801a1d4b63855b744ac93e61a08182f212a
AlexanderHaan <haan.alex@outlook.de>
2022-06-21 09:44:52 +0200

    dont show signs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,6 +7,11 @@ vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', op
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
+-- Diagnostics View
+vim.diagnostic.config({
+    signs = false,
+})
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
