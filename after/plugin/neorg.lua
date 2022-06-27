a50a5020281af7a1140deed8478e3d3c5fa984e1
AlexanderHaan <haan.alex@outlook.de>
2022-06-27 13:37:51 +0200

    add gtd and keybindings

--- after/plugin/neorg.lua
+++ after/plugin/neorg.lua
@@ -9,6 +9,20 @@ require('neorg').setup {
                 }
             }
         },
-        ["core.norg.concealer"] = {}
+        ["core.norg.concealer"] = {},
+        ["core.gtd.base"] = {
+            config = {
+                workspace = "work"
+            }
+        },
+        ["core.keybinds"] = {
+            config = {
+                hook = function(keybinds)
+                    keybinds.remap_event("norg", "n", "gntc", "core.gtd.base.capture")
+                    keybinds.remap_event("norg", "n", "gntv", "core.gtd.base.views")
+                    keybinds.remap_event("norg", "n", "gnte", "core.gtd.base.edit")
+                end
+            }
+        }
     }
 }
