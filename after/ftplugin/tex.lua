f4161577b306384920e9a0b25c381532227c58a9
alexanderhaanpartner <alexander.haan@partner.bmwgroup.com>
2023-04-20 10:18:07 +0200

    add markdown plugin

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -12,6 +12,9 @@ vim.api.nvim_buf_set_keymap(
 set.wrap = true
 set.linebreak = true
 
+-- Color column
+set.colorcolumn = { 80 } 
+
 -- Mapping for movement in paragraph
 vim.api.nvim_buf_set_keymap(0, 'n', 'j', 'gj', {noremap = true})
 vim.api.nvim_buf_set_keymap(0, 'n', 'k', 'gk', {noremap = true})
--- init.lua
+++ init.lua
@@ -18,6 +18,8 @@ require('packer').startup(function()
     use 'MaxMEllon/vim-jsx-pretty' -- React plugin
     use 'L3MON4D3/LuaSnip' -- Snippet engine
     use 'nvim-treesitter/nvim-treesitter' -- Treesitter
+    use 'godlygeek/tabular' -- For markdown
+    use 'preservim/vim-markdown' -- For markdown
 end)
 
 vim.cmd('filetype plugin indent on') -- Enable filetype
@@ -88,3 +90,28 @@ set.foldlevelstart = 99 -- no folds at beginning
 
 -- Colorscheme
 vim.cmd('colorscheme dracula')
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "c", "lua", "vim", "query" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
